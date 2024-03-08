e4d52ce080faf08b9fb3e363f202fcafe92f0e4c
ismael <maelio@live.com>
2024-03-07 23:21:36 -0300

    next

--- src/pages/ErrorPage/styles.js
+++ src/pages/ErrorPage/styles.js
@@ -17,7 +17,8 @@ export const Container = styled.div`
   flex-direction: column;
   align-items: center;
   justify-content: center;
-  height: 100vh;
+  height: 70vh;
+
   text-align: center;
 
   img {
